41a77f6aa21a29982fda89eea47ebe5566e30443
samuelthapa2024 <samuelthapa.spell@gmail.com>
2024-02-02 15:19:29 +0545

    go to specific page

--- lib/src/book_pdf/book_pdf.dart
+++ lib/src/book_pdf/book_pdf.dart
@@ -4,6 +4,7 @@ import 'package:alc_book/src/models/book_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
@@ -28,18 +29,19 @@ class BookPDF extends StatefulWidget {
 
 class _BookPDFState extends State<BookPDF> {
   bool status = true;
-  bool showOptionsBar = true;
+  bool showOptionsBar = false;
 
   late PDFViewController _pdfViewController;
   final TextEditingController _pageNumberController = TextEditingController();
-  final ScrollController _scrollController = ScrollController();
 
   int? totalPages;
 
   @override
   void initState() {
     super.initState();
-    WakelockPlus.enable(); //To keep screen awake
+    WakelockPlus.enable();
+
+    //To keep screen awake
     // _checkStatus();
   }
 
@@ -52,35 +54,19 @@ class _BookPDFState extends State<BookPDF> {
     setState(() {});
   }
 
-  // void _handleDragUpdate(DragUpdateDetails details) {
-  //   // Use details.delta.dy to determine the scroll direction and magnitude
-  //   // Update UI or perform other actions as needed
-  //   _scrollController.addListener(() {
-  //     final scrollDirection = _scrollController.position.userScrollDirection;
-  //     if (scrollDirection == ScrollDirection.forward) {
-  //       print('Scrolling up');
-  //       showOptionsBar = false;
-  //       setState(() {});
-  //     }
-  //     if (scrollDirection == ScrollDirection.reverse) {
-  //       print('Scrolling down');
-  //       showOptionsBar = true;
-  //       setState(() {});
-  //     }
-  //   });
-  // }
-
   @override
   void dispose() {
     _pageNumberController.dispose();
-    _scrollController.dispose();
+
     WakelockPlus.disable(); //To disable screen awake
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    // print('reloded');
     return Scaffold(
       backgroundColor: AppColors.white,
       appBar: AppBar(
@@ -155,6 +141,7 @@ class _BookPDFState extends State<BookPDF> {
       body: SafeArea(
         child: Stack(
           alignment: Alignment.topCenter,
+          // fit: StackFit.expand,
           children: [
             Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Expanded(
@@ -162,13 +149,6 @@ class _BookPDFState extends State<BookPDF> {
                     fitEachPage: false,
                     pageSnap: false,
                     pageFling: false,
-                    // nightMode: true,
-                    // gestureRecognizers: Set()
-                    //   ..add(
-                    //     Factory<VerticalDragGestureRecognizer>(() =>
-                    //         VerticalDragGestureRecognizer()
-                    //           ..onUpdate = _handleDragUpdate),
-                    //   ),
                     onViewCreated: (PDFViewController pdfViewController) {
                       _pdfViewController = pdfViewController;
                     },
@@ -178,6 +158,7 @@ class _BookPDFState extends State<BookPDF> {
                       });
                     }).cachedFromUrl(
                   widget.book.book,
+                  maxAgeCacheObject: const Duration(days: 365),
                   placeholder: (progress) => Center(
                     child: CircularProgressIndicator(
                       value: progress / 100,
@@ -213,75 +194,76 @@ class _BookPDFState extends State<BookPDF> {
             ]),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: AnimatedContainer(
-                // height: 40,
-                height: showOptionsBar ? 50 : 0,
-                width: 100.w,
-                duration: const Duration(milliseconds: 500),
-                constraints: const BoxConstraints(maxHeight: 70, maxWidth: 150),
-                decoration: BoxDecoration(
-                  color: AppColors.background.withOpacity(0.8),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: SizedBox(
-                        width: width / 9,
-                        height: 40,
-                        child: TextFormField(
-                          // initialValue: '0',
-                          scrollPadding: EdgeInsets.zero,
-                          keyboardType: TextInputType.number,
-                          controller: _pageNumberController,
-                          style: TextStyle(fontSize: 12, color: Colors.black),
-                          onTapOutside: (pointerDownEvent) {
-                            FocusScopeNode currentFocus =
-                                FocusScope.of(context);
-                            if (!currentFocus.hasPrimaryFocus &&
-                                currentFocus.focusedChild != null) {
-                              currentFocus.focusedChild?.unfocus();
-                            }
-                          },
-                          onFieldSubmitted: (_) {
-                            int goToPage =
-                                int.parse(_pageNumberController.text) - 1;
-                            _pdfViewController.setPage(goToPage);
-                          },
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColors.primary),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColors.primary),
-                              borderRadius: BorderRadius.circular(8),
+              child: GestureDetector(
+                onTap: () {
+                  if (showOptionsBar == false) {
+                    setState(() {
+                      showOptionsBar = true;
+                    });
+                  }
+                },
+                child: Container(
+                  height: 40,
+                  width: 90,
+                  // duration: const Duration(milliseconds: 500),
+                  constraints:
+                      const BoxConstraints(maxHeight: 70, maxWidth: 150),
+                  decoration: BoxDecoration(
+                    color: AppColors.primary.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: TextFormField(
+                            // initialValue: '0',
+                            scrollPadding: EdgeInsets.zero,
+                            keyboardType: TextInputType.number,
+                            controller: _pageNumberController,
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                            onTapOutside: (pointerDownEvent) {
+                              FocusScopeNode currentFocus =
+                                  FocusScope.of(context);
+                              if (!currentFocus.hasPrimaryFocus &&
+                                  currentFocus.focusedChild != null) {
+                                currentFocus.focusedChild?.unfocus();
+                              }
+                            },
+                            onFieldSubmitted: (_) {
+                              int goToPage =
+                                  int.parse(_pageNumberController.text) - 1;
+                              _pdfViewController.setPage(goToPage);
+                            },
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.primary),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.primary),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Text('of ${totalPages == null ? '' : '$totalPages'}'),
-                    SizedBox(width: 5)
-                  ],
+                      Text('of ${totalPages == null ? '' : '$totalPages'}'),
+                      SizedBox(width: 5)
+                    ],
+                  ),
                 ),
               ),
             ),
           ],
         ),
       ),
-      // body: _isLoading
-      //     ? const Center(child: CircularProgressIndicator())
-      //     : PDFViewer(
-      //         document: _document,
-      //         scrollDirection: Axis.vertical,
-      //         enableSwipeNavigation: true,
-      //         lazyLoad: true,
-      //         indicatorBackground: AppColors.red,
-      //         pickerButtonColor: AppColors.red,
-      //       ),
     );
   }
 }
